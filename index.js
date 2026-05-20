7d16c406ee9e3169b1804f255f1f21b2bccbc45f
Ruchir Kale <ruchirkale011@gmail.com>
2026-05-20 11:29:13 +0530

    Add files via upload
    Fixed certificates sizes 
    changed about section 
    added projects and their UI

---
+++ index.js
@@ -0,0 +1,18 @@
+// displaying hidden text
+function showItem() {
+  let x = document.getElementById("infoElement");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showItem_2() {
+  let x = document.getElementById("infoElement2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}

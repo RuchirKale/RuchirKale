7e0f92d65a270e7ade2e04bfdd72cfaa5913d729
Ruchir Kale <ruchirkale011@gmail.com>
2026-05-30 21:06:41 +0530

    Update index.js
    added the search engine info giving

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ function showItem_2() {
     x.style.display = "none";
   }
 }
+//recognition for google and other search engines
+function addStructuredData() {
+    const schema = {
+        "@context": "https://schema.org",
+        "@type": "Person",
+        "name": "Ruchir Kale",
+        "url": "https://ruchirkale.github.io",
+        "jobTitle": "Student Developer",
+        "description": "Machine Learning and Embedded Systems enthusiast",
+        "sameAs": [
+            "https://github.com/RuchirKale",
+            "https://www.kaggle.com/ruchirkale",
+            "https://x.com/RuchirKale"
+        ]
+    };
+
+    const script = document.createElement("script");
+    script.type = "application/ld+json";
+    script.textContent = JSON.stringify(schema);
+
+    document.head.appendChild(script);
+}
+
+addStructuredData();
